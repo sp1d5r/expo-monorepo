8f2f143c7024f306f70f1571d820b1d6084e8596
Elijah <elijahahmad03@gmail.com>
2025-02-27 20:21:55 +0000

    Frontend: Added divider.

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,10 +2,11 @@ import { Avatar } from './avatar';
 import { Badge } from './badge';
 import { Button } from './button';
 import { Checkbox } from './checkbox';
+import { Divider } from './divider';
 import { Icon } from './icon';
 import { Input } from './input';
 import { Switch } from './switch';
 import tamaguiConfig from './tamagui.config';
 import { Text } from './text';
 
-export { Avatar, Badge, Button, tamaguiConfig, Text, Icon, Input, Checkbox, Switch };
+export { Avatar, Badge, Button, Divider, tamaguiConfig, Text, Icon, Input, Checkbox, Switch };
