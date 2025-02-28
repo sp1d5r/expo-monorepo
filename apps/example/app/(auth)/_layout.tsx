41b43f82a56f7f58b22a134cea2e91b030e216b9
Elijah <elijahahmad03@gmail.com>
2025-02-28 20:26:04 +0000

    Frontend: Added authentication redirect.

--- apps/example/app/(auth)/_layout.tsx
+++ apps/example/app/(auth)/_layout.tsx
@@ -1,6 +1,17 @@
-import { Stack } from 'expo-router';
+import { router, Stack } from 'expo-router';
+import { useEffect } from 'react';
+
+import { useAuth } from '@/contexts/AuthContext';
 
 export default function AuthLayout() {
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (user && user.email) {
+      router.replace('/(tabs)');
+    }
+  }, [user]);
+
   return (
     <Stack>
       <Stack.Screen
